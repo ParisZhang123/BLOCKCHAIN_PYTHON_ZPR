92e7e0c6570d12737c0ff29ec228e05e3e01d8a0
ParisZhang123 <86837854+ParisZhang123@users.noreply.github.com>
2021-11-14 22:16:29 +1100

    Add files via upload

---
+++ wallet/constants.py
@@ -0,0 +1,63 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "c67f411a-d674-4c8b-94b5-0a3483a80e8f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "BTC = 'btc'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "f07fce66-0958-48d2-80d0-16b8911e5922",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "ETH = 'eth'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "6bb29db4-b22e-408c-996f-b69246e8b5cb",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "BTCTEST = 'btc-test'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "058d712c-e1a4-4d3b-bdf0-856e4239a35e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "dev",
+   "language": "python",
+   "name": "dev"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
